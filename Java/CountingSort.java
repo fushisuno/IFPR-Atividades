2ee77967d607c46e291816c4102bba0b65cbf344
fushisuno <114326061+fushisuno@users.noreply.github.com>
2023-11-07 23:19:35 +0000

    #Title: Aula de Algoritmo

---
+++ Java/CountingSort.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CountingSort{
+  static Scanner ler = new Scanner(System.in);
+  public static void main(String[] args){
+    int[] vet = new int[8];
+    for (int i = 0; i < vet.length; i++) {
+        vet[i] = ler.nextInt();
+    }
+
+  }
+
+  public static void counting(int vet[]){
+      int aux[] = new int[vet.length];
+      for (int i = 0; i < vet.length; i++) {
+        for (int j = 0; j < aux.length; j++) {
+          
+        }
+      }
+  }
+}
