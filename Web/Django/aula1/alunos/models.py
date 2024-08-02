280e57ca9a3ab8569f9092b37024574e74590a7d
Kainã Magdiel <kainamagdiel1920@gmail.com>
2024-08-02 23:36:25 +0000

    #Title: Trabalhando com relacionamentos NxM #Changes: Realizando a inserção de dados no banco, e aprendendo a utilizar metodos de ralacionamento NxM em Django, aprendendo também a realizar algumas consultas.

--- Web/Django/aula1/alunos/models.py
+++ Web/Django/aula1/alunos/models.py
@@ -8,13 +8,24 @@ class Aluno(models.Model):
   nome = models.CharField(max_length=200)
   data_nascimento = models.DateField()
 
+class Professor(models.Model):
+  nome = models.CharField(max_length=200)
+
 class Curso(models.Model):
   nome = models.CharField(max_length=200)
 
   def __str__(self):
     return self.nome
 
-class Diciplina(models.Model):
+class Disciplina(models.Model):
   curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+  professor = models.ForeignKey(Professor, on_delete=models.CASCADE, null=True)
   nome = models.CharField(max_length=200)
   codigo = models.CharField(max_length=6, null=True)
+
+  alunos = models.ManyToManyField(Aluno)
+
+class Matricula (models.Model):
+  aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+  disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+  periodo = models.IntegerField(null=True)
