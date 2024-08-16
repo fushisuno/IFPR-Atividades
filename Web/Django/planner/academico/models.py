7167d6fe1892ec27982b6c03d9c57411f3510d19
Kainã Magdiel <kainamagdiel1920@gmail.com>
2024-08-16 23:30:05 +0000

    #Title: API REST #Changes: Aprendendo a criar uma api rest usando Django, uma api simples, que realiza o post de 2 valores: "Nome"; "Email";

---
+++ Web/Django/planner/academico/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Professor(models.Model):
+  nome = models.CharField(max_length=200),
+  email = models.CharField(max_length=200),
+  def __str__(self):
+    return self.nome
+
+class Curso(models.Model):
+  nome = models.CharField(max_length=200),
+  sigla = models.CharField(max_length=200),
+
+  def __str__(self):
+    return self.nome
+
+    
