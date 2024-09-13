d34ca9c2225a073875ded5b84a6c229d301a74cb
Kainã Magdiel <114326061+fushisuno@users.noreply.github.com>
2024-09-13 23:03:27 +0000

    #Title: Atualizando a API

--- Web/Django/planner/academico/models.py
+++ Web/Django/planner/academico/models.py
@@ -1,17 +1,26 @@
+# https://docs.djangoproject.com/en/5.0/topics/db/models/
+# https://docs.djangoproject.com/en/5.0/ref/models/fields/
+
 from django.db import models
 
-# Create your models here.
+
 class Professor(models.Model):
-  nome = models.CharField(max_length=200),
-  email = models.CharField(max_length=200),
-  def __str__(self):
-    return self.nome
+    nome = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nome
 
 class Curso(models.Model):
-  nome = models.CharField(max_length=200),
-  sigla = models.CharField(max_length=200),
+    nome = models.CharField(max_length=200)
+    sigla = models.CharField(max_length=12)
 
-  def __str__(self):
-    return self.nome
+    def __str__(self):
+        return self.nome
 
-    
+
+class Disciplina(models.Model):
+    #chave estrangeira para Curso.
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, null=True)
+    nome = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=6, null=True)
